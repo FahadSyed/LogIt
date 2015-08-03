a6db16ccd203fe51a3f55be8122d3c31b1307996
FahadSyed <fahadsyed1995@gmail.com>
2015-08-03 15:34:42 -0400

    - Datetime saving, event saved - Version 1.0 complete

--- app/src/main/java/com/fahadalisyed/logit/Home/Confirm.java
+++ app/src/main/java/com/fahadalisyed/logit/Home/Confirm.java
@@ -1,6 +1,12 @@
 package com.fahadalisyed.logit.Home;
 
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +15,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fahadalisyed.logit.Log.LogItem;
 import com.fahadalisyed.logit.Log.LogItemManager;
 import com.fahadalisyed.logit.R;
 import com.fahadalisyed.logit.Utilities.TimeFormat;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.calendar.CalendarScopes;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class Confirm extends ActionBarActivity {
@@ -43,6 +60,21 @@ public class Confirm extends ActionBarActivity {
     private TextView m_endTimeTV;
 
     private LogItemManager m_logItemManager;
+
+    // Google Calendar
+
+    com.google.api.services.calendar.Calendar mService;
+
+    GoogleAccountCredential credential;
+    final HttpTransport transport = AndroidHttp.newCompatibleTransport();
+    final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    private static final String[] SCOPES = { CalendarScopes.CALENDAR };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +87,22 @@ public class Confirm extends ActionBarActivity {
         setViews();
 
         m_logItemManager = LogItemManager.getInstance();
+
+        // Calendar
+
+        // Initialize credentials and service object.
+        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+        credential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff())
+                .setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME, null));
+
+        mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName("Google Calendar API Android Quickstart")
+                .build();
+
+
     }
 
     private void extractIntent( Intent intent ) {
@@ -81,8 +129,8 @@ public class Confirm extends ActionBarActivity {
         m_startDateTV.setText( TimeFormat.formatDate(m_startDate) );
         m_endDateTV.setText( TimeFormat.formatDate(m_endDate) );
 
-        m_startTimeTV.setText( TimeFormat.formatDateTime(m_startDate) );
-        m_endTimeTV.setText( TimeFormat.formatDateTime(m_endDate) );
+        m_startTimeTV.setText(TimeFormat.formatDateTime(m_startDate));
+        m_endTimeTV.setText(TimeFormat.formatDateTime(m_endDate));
         m_durationTV.setText( TimeFormat.formatElapsedTime(m_duration) );
     }
 
@@ -98,7 +146,144 @@ public class Confirm extends ActionBarActivity {
                 m_duration
         );
 
-        m_logItemManager.printLogItem( item );
+        refreshResults(item);
+        m_logItemManager.printLogItem(item);
+    }
+
+    /**
+     * Called when an activity launched here (specifically, AccountPicker
+     * and authorization) exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it.
+     * @param requestCode code indicating which activity result is incoming.
+     * @param resultCode code indicating the result of the incoming
+     *     activity result.
+     * @param data Intent (containing result data) returned by incoming
+     *     activity result.
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    isGooglePlayServicesAvailable();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        credential.setSelectedAccountName(accountName);
+                        SharedPreferences settings =
+                                getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.commit();
+                    }
+                } else if (resultCode == RESULT_CANCELED) {
+                    updateStatus("Account unspecified");
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode != RESULT_OK) {
+                    chooseAccount();
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * Attempt to save to Google Calendar, if the
+     * email address isn't known yet, then call chooseAccount()
+     * method so the user can pick an account.
+     */
+    private void refreshResults( LogItem itemToSave ) {
+        if (credential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else {
+            if (isDeviceOnline()) {
+                new ApiAsyncTask(this, itemToSave).execute();
+            } else {
+                updateStatus("No network connection available");
+            }
+        }
+    }
+
+    /**
+     * Show a status message in the list header TextView; called from background
+     * threads and async tasks that need to update the UI (in the UI thread).
+     * @param message a String to display in the UI header TextView.
+     */
+    public void updateStatus(final String message) {
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Checks whether the device currently has a network connection.
+     * @return true if the device has a network connection, false otherwise.
+     */
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    /**
+     * Starts the activity so the user can choose an account.
+     */
+    private void chooseAccount() {
+        startActivityForResult(
+                credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+    }
+
+    /**
+     * Check that Google Play services APK is installed and up to date. Will
+     * launch an error dialog for the user to update Google Play Services if
+     * possible.
+     * @return true if Google Play Services is available and up to
+     *     date on this device; false otherwise.
+     */
+    private boolean isGooglePlayServicesAvailable() {
+        final int connectionStatusCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+            return false;
+        } else if (connectionStatusCode != ConnectionResult.SUCCESS ) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     *     Google Play Services on this device.
+     */
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                        connectionStatusCode,
+                        Confirm.this,
+                        REQUEST_GOOGLE_PLAY_SERVICES);
+                dialog.show();
+            }
+        });
     }
 
     @Override
